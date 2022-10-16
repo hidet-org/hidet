88384ba1538388a59a4283e373dbd096111f5ff3
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2022-10-16 02:32:03 -0400

    remove unnecessary dependencies

--- config.cmake
+++ config.cmake
@@ -1,27 +1,4 @@
-# Set the compute capacity of target GPU.
-# A lower number is compatible with newer GPUs. Compute capacity of typical NVIDIA GPUs:
-#   - 60: P100
-#   - 70: V100
-#   - 80: A100
-#   - 61: RTX 10 Series
-#   - 75: RTX 20 Series
-#   - 86: RTX 30 Series
-set(HIDET_CUDA_ARCH 60)
-
 # Set build type
 #  - Debug
 #  - Release
 set(HIDET_BUILD_TYPE Release)
-
-# Whether to build TVM.
-# Hidet does not depends on TVM, but we can build it to do benchmarking and comparison.
-#  - ON: Build TVM
-#  - OFF: Do not build TVM
-set(HIDET_BUILD_TVM OFF)
-
-# Whether to build cudnn baseline
-#  - ON: Build cudnn baseline, and search it automatically.
-#  - A-Path-to-cuDNN: Use the given path to search cudnn library. Can be used to specify
-#                     different version of cuDNN.
-#  - OFF: Do not build cudnn baseline.
-set(HIDET_BASELINE_CUDNN OFF)
--- scripts/reset_gpu_clocks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. "$(dirname "$0")/query_gpu_status.sh"
-
-echo "Resetting GPU to clocks, requires sudo privilege."
-sudo -S nvidia-smi --reset-memory-clocks > /dev/null
-sudo -S nvidia-smi --reset-gpu-clocks > /dev/null
-
-. "$(dirname "$0")/query_gpu_status.sh"
