c8843c4542e0907e3de3e3169032e7f355583a46
Hanjie <50634613+hjjq@users.noreply.github.com>
2022-11-03 18:08:15 -0400

    [Operator] Add PRelu op (#8)

--- python/hidet/graph/ops/__init__.py
+++ python/hidet/graph/ops/__init__.py
@@ -6,7 +6,7 @@ from .definitions.conv2d_transpose import conv2d_transpose
 from .definitions.matmul import batch_matmul, matmul
 from .definitions.pool import max_pool2d, avg_pool2d
 from .definitions.softmax import softmax
-from .definitions.activation import relu, leaky_relu, sigmoid, clip, relu6
+from .definitions.activation import relu, leaky_relu, sigmoid, clip, relu6, prelu
 from .definitions.norm import batch_norm_infer, instance_norm, layer_norm
 from .definitions.image import resize2d
 from .definitions.arithmatic import add, sub, multiply, divide, neg, sqrt, rsqrt, sin, cos, pow, erf, tanh, where
