1ca3a089f828c2b3e1d728c9fa40f7487bd49243
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2022-11-04 01:35:37 -0400

    fix typo

--- python/hidet/graph/transforms/graph_patterns/__init__.py
+++ python/hidet/graph/transforms/graph_patterns/__init__.py
@@ -1,10 +1,10 @@
 from typing import List
 from .base import TensorPattern, OperatorPattern, GraphPattern, MatchDict, Usage, graph_pattern_match
-from .arithmatic_patterns import arithmatic_patterns
+from .arithmetic_patterns import arithmetic_patterns
 from .transform_patterns import transform_patterns
 from .conv2d_patterns import conv2d_patterns
 from .matmul_patterns import matmul_patterns
 
 
 def all_graph_patterns() -> List[GraphPattern]:
-    return arithmatic_patterns() + transform_patterns() + conv2d_patterns() + matmul_patterns()
+    return arithmetic_patterns() + transform_patterns() + conv2d_patterns() + matmul_patterns()
