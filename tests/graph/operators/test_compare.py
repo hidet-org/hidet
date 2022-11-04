2c2595de9793c2c8aeffd8f5cc209fbe26ed867d
hjjq <hanjie.qiu@mail.utoronto.ca>
2022-11-04 17:24:22 -0400

    Reformat prev commit

--- tests/graph/operators/test_compare.py
+++ tests/graph/operators/test_compare.py
@@ -4,24 +4,13 @@ import pytest
 from hidet import ops
 from hidet.testing import check_unary, check_binary
 
+
 @pytest.mark.parametrize("a_shape, b_shape, dtype", [[[33, 44], [44], "bool"]])
 def test_and(a_shape, b_shape, dtype):
     # without broadcast
-    check_binary(
-        a_shape,
-        a_shape,
-        lambda a, b: np.logical_and(a, b),
-        lambda a, b: ops.cond_and(a, b),
-        dtype=dtype,
-    )
+    check_binary(a_shape, a_shape, lambda a, b: np.logical_and(a, b), lambda a, b: ops.cond_and(a, b), dtype=dtype)
     # with broadcast
-    check_binary(
-        a_shape,
-        b_shape,
-        lambda a, b: np.logical_and(a, b),
-        lambda a, b: ops.cond_and(a, b),
-        dtype=dtype,
-    )
+    check_binary(a_shape, b_shape, lambda a, b: np.logical_and(a, b), lambda a, b: ops.cond_and(a, b), dtype=dtype)
 
 
 if __name__ == '__main__':
