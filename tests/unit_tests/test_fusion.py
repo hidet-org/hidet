61326e0cefcdc29e21979b5ae3bb090b06391b24
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2022-11-08 18:45:50 -0500

    [Functor] Fix a bug in IR functor (#15)
    * fix
    
    * format

---
+++ tests/unit_tests/test_fusion.py
@@ -0,0 +1,18 @@
+import hidet
+
+
+def test_fusion_v1():
+    @hidet.jit(opt=True)
+    def func(a: hidet.Tensor, b: hidet.Tensor):
+        c = hidet.ops.equal(a, b)
+        d = hidet.ops.cond_not(c)
+        e = d.cast('int32')
+        f = hidet.ops.cumsum(e, dim=1)
+        g = f * e
+        h = g.cast('int64')
+        i = h + 1
+        return i
+
+    a = hidet.zeros([1, 9], dtype='int64')
+    b = hidet.zeros([], dtype='int64')
+    func(a, b)
