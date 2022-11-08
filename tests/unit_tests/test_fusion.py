e7469a997a7bd529d20d1668109c3e62f677a268
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2022-11-08 18:05:06 -0500

    format

--- tests/unit_tests/test_fusion.py
+++ tests/unit_tests/test_fusion.py
@@ -4,7 +4,7 @@ import hidet
 def test_fusion_v1():
     @hidet.jit(opt=True)
     def func(a: hidet.Tensor, b: hidet.Tensor):
-        c = hidet.ops.equal(a,  b)
+        c = hidet.ops.equal(a, b)
         d = hidet.ops.cond_not(c)
         e = d.cast('int32')
         f = hidet.ops.cumsum(e, dim=1)
@@ -16,4 +16,3 @@ def test_fusion_v1():
     a = hidet.zeros([1, 9], dtype='int64')
     b = hidet.zeros([], dtype='int64')
     func(a, b)
-
