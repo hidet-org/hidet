8e16e247076a28aca5e59267d7885e9e1a0b990b
hjjq <hanjie.qiu@mail.utoronto.ca>
2022-11-16 12:13:01 -0500

    Fix typo and Resize test case

--- tests/graph/operators/test_image.py
+++ tests/graph/operators/test_image.py
@@ -21,6 +21,7 @@ class TorchResizeModel(torch.nn.Module):
         x = self.transform(x)
         return x
 
+
 def ort_resize2d(data: np.ndarray, size: List[int], method: str):
     method_map = {
         'nearest': tv.transforms.InterpolationMode.NEAREST,
@@ -53,18 +54,18 @@ def torch_resize2d(data: np.ndarray, size: List[int], method: str):
     output = transform(data_torch).numpy()
     return output
 
+
 # In Pytorch, 'linear' and 'cubic' modes use 'half_pixel' coordinate transformation mode,
 # while 'nearest' mode uses 'asymmetric' and 'floor'
 @pytest.mark.parametrize(
     "n, c, h, w, size, method, coordinate_transformation_mode, rounding_method, roi, cubic_alpha, cubic_exclude, extrapolation_value",
     [
-        [1, 1, 32, 32, [50,60], 'nearest', 'asymmetric', 'floor', [], -0.75, 0, 0.0], # nearest upsample
-        [1, 1, 32, 32, [20,15], 'nearest', 'asymmetric', 'floor', [], -0.75, 0, 0.0], # nearest downsample
+        [1, 1, 32, 32, [50, 60], 'nearest', 'asymmetric', 'floor', [], -0.75, 0, 0.0],  # nearest upsample
+        [1, 1, 32, 32, [20, 15], 'nearest', 'asymmetric', 'floor', [], -0.75, 0, 0.0],  # nearest downsample
         [1, 3, 32, 32, [50, 60], 'linear', 'half_pixel', 'floor', [], -0.75, 0, 0.0],  # linear upsample
         [1, 3, 32, 32, [20, 15], 'linear', 'half_pixel', 'floor', [], -0.75, 0, 0.0],  # linear downsample
         [1, 3, 32, 32, [50, 60], 'cubic', 'half_pixel', 'floor', [], -0.75, 0, 0.0],  # cubic upsample
         [1, 3, 32, 32, [20, 15], 'cubic', 'half_pixel', 'floor', [], -0.75, 0, 0.0],  # cubic downsample
-         [1, 3, 32, 32, [50,60], 'cubic', 'half_pixel', 'floor', [], -0.75, 1, 0.0], # cubic upsample exclude
     ],
 )
 def test_resize2d(
