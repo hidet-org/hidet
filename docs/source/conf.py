7b55f9df08d81a35dd36b78a841efddfc20f5c25
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2022-12-01 03:16:10 +0800

    [Docs] Change github repo url (#25)
    change github repo url from yaoyaoding/hidet to hidet-rog/hidet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,7 +103,7 @@ html_favicon = '_static/favicon.svg'
 html_theme = 'sphinx_book_theme'
 # html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    "repository_url": "https://github.com/yaoyaoding/hidet",
+    "repository_url": "https://github.com/hidet-org/hidet",
     "use_repository_button": True,
     'logo_only': True,
     "extra_navbar": r"<a href=/netron target=_blank>Customized Netron</a>",
