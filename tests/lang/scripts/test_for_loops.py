3bcb472342a886e1f1296ff455ae0d455eb717a8
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2022-12-13 15:45:33 -0500

    .

--- tests/lang/scripts/test_for_loops.py
+++ tests/lang/scripts/test_for_loops.py
@@ -1,72 +1,75 @@
+from typing import List
+import numpy as np
 import pytest
 import hidet
+from hidet import int32
 from hidet.ir.func import IRModule, Function
 from hidet.transforms.tools import add_packed_func
-
 from hidet.lang import attr, printf, grid, spatial
 
 
-def check(kernel: Function, output: str, capfd):
-    with capfd.disabled():
-        ir_module = IRModule()
-        ir_module.add(kernel.name, kernel)
-        add_packed_func(ir_module, kernel, 'func')
-        func = hidet.driver.build_ir_module(ir_module, func_name='func')
-    func()
-    captured = capfd.readouterr()
-    assert captured.out == output
+def run(kernel: Function, shape: List[int]) -> hidet.Tensor:
+    ir_module = IRModule()
+    ir_module.add(kernel.name, kernel)
+    add_packed_func(ir_module, kernel, 'func')
+    func = hidet.driver.build_ir_module(ir_module, func_name='func')
+
+    a = hidet.empty(shape, dtype=hidet.int32).cpu()
+    func(a)
+    return a
 
 
-def test_for_range(capfd):
+def test_for_range():
     @hidet.script
-    def kernel():
+    def kernel(a: int32[10]):
         attr.func_kind = 'host_kernel'
+        p = 0
         for i in range(10):
-            printf('%d ', i)
+            a[i] = p
+            p = p + 1
 
-    check(kernel, '0 1 2 3 4 5 6 7 8 9 ', capfd)
+    output = run(kernel, [10])
+    assert np.all(output.numpy() == np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
 
-def test_for_grid(capfd):
+def test_for_grid():
     @hidet.script
-    def kernel():
+    def kernel(a: int32[2, 3]):
         attr.func_kind = 'host_kernel'
+        p = 0
         for i, j in grid(2, 3):
-            printf(r'%d %d\n', i, j)
+            a[i, j] = p
+            p += 1
 
-    expected = ''
-    for i in range(2):
-        for j in range(3):
-            expected += '{} {}\n'.format(i, j)
-
-    check(kernel, expected, capfd)
+    expected = np.array([[0, 1, 2], [3, 4, 5]])
+    assert np.all(run(kernel, [2, 3]).numpy() == expected)
 
 
-def test_for_task_mapping(capfd):
+def test_for_task_mapping():
     @hidet.script
-    def kernel():
+    def kernel(a: int32[2, 3]):
         attr.func_kind = 'host_kernel'
+        p = 0
         for w in range(6):
             for i, j in spatial(2, 3).on(w):
-                printf(r'%d %d\n', i, j)
+                a[i, j] = p
+            p += 1
 
-    expected = ''
-    for w in range(6):
-        i, j = w // 3, w % 3
-        expected += '{} {}\n'.format(i, j)
-    check(kernel, expected, capfd)
+    expected = np.array([[0, 1, 2], [3, 4, 5]])
+    assert np.all(run(kernel, [2, 3]).numpy() == expected)
 
 
-def test_tuple_as_index(capfd):
+def test_tuple_as_index():
     @hidet.script
-    def kernel():
+    def kernel(a: int32[2, 3]):
         attr.func_kind = 'host_kernel'
+        p = 0
         for axes in grid(2, 3):
-            printf(r'%d %d\n', axes[0], axes[1])
+            # the following three ways of indexing are equivalent
+            a[axes] = p
+            a[axes[0], axes[1]] = p
+            a[axes[0]][axes[1]] = p
+            p += 1
 
-    expected = ''
-    for i in range(2):
-        for j in range(3):
-            expected += '{} {}\n'.format(i, j)
-
-    check(kernel, expected, capfd)
+    expected = np.array([[0, 1, 2], [3, 4, 5]])
+    assert np.all(run(kernel, [2, 3]).numpy() == expected)
