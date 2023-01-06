582d57382410a534a54dd3b3b4930555297aac97
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-01-05 23:37:32 -0500

    [Version] Bump version to 0.2.dev (#60)
    .

--- scripts/upload_wheel.sh
+++ scripts/upload_wheel.sh
@@ -4,5 +4,6 @@ WHEEL=$1
 
 set -e  # exit immediately if a command exits with a non-zero status.
 
-twine upload --repository testpypi $WHEEL
+# twine upload --repository testpypi $WHEEL
+twine upload $WHEEL
 
