a30d56d0de916386007a48dd063706676f79f5cd
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-01-08 18:42:56 -0500

    .

--- scripts/wheel/current_version.py
+++ scripts/wheel/current_version.py
@@ -9,6 +9,7 @@ $ python scripts/wheel/current_version.py
 """
 import os
 import argparse
+import datetime
 
 parser = argparse.ArgumentParser("current_version.py")
 
@@ -17,6 +18,11 @@ parser.add_argument(
     type=str,
     default="./",
     help="root directory of the project, under which setup.py is located. Default: ./",
+)
+parser.add_argument(
+    "--nightly",
+    action="store_true",
+    help="If set, the version will be set to the nightly version. ",
 )
 
 
@@ -29,13 +35,21 @@ def main():
     )
     if not os.path.exists(version_py) or not os.path.isfile(version_py):
         raise FileNotFoundError(version_py)
+    output_version = None
     with open(version_py, "r") as f:
         lines = f.readlines()
         for line in lines:
             if line.startswith("__version__ = "):
                 version = line.split("=")[1].strip()[1:-1]
-                print(version)
+                if args.nightly:
+                    date_string = datetime.datetime.now().strftime("%Y%m%d")
+                    if version.endswith(".dev"):
+                        version = version.replace(".dev", ".dev{}".format(date_string))
+                output_version = version
                 break
+    if output_version is None:
+        raise RuntimeError('The occurrence of "__version__ = " in version.py is not')
+    print(output_version, end="")
 
 
 if __name__ == "__main__":
