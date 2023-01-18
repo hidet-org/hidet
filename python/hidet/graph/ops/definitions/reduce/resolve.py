f93646a141b3b187885431f9507f872736d100a6
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-01-18 18:39:49 -0500

    Refactor reduce and add resolve for reduce

---
+++ python/hidet/graph/ops/definitions/reduce/resolve.py
@@ -0,0 +1,77 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import List, Optional, Callable, Any
+from functools import lru_cache
+
+import hidet.cuda
+from hidet.ir import dtypes
+from hidet.graph.ir import Operator, Tensor
+from hidet.graph.transforms import ResolveRule, register_resolve_rule
+from hidet.utils.py import gcd, factorize, prod, cdiv
+
+from .reduce import ReduceBaseOp
+from .reduce_f16 import reduce_f16
+from ..transform import broadcast, flatten
+from ..utils import broadcast_shapes
+
+
+@register_resolve_rule(ReduceBaseOp)
+class ReduceResolveRule(ResolveRule):
+    """
+    Resolve a generic reduce operator according to the following rules, in decreasing priority:
+    1) resolve_simplify: If the size of all reduce dimensions are 1, return the input itself if keepdims=True,
+        else return Squeeze(input, axis=reduce_dims).
+    2) resolve_f16: If the input data type is float16, and the size of the last input dimension is an even number,
+        return the output of the f16 optimized reduce schedule. (Support of odd number will be added in the future)
+    3) resolve_generic: Default case, return the output of the f32 optimized reduce schedule.
+        Currently those schedules
+    """
+    def resolve_simplify(self, op: Operator) -> Optional[List[Tensor]]:
+        dims = op.attrs['dims']
+        keepdims = op.attrs['keepdims']
+        x: Tensor = op.inputs[0]
+        shape = x.shape
+        print("in resolve_simplify")
+        if not all(shape[d] == 1 for d in dims):
+            return None
+        if keepdims:
+            print("returning self")
+            return [x]
+        print("returning squeezed self")
+        return [x.squeeze(dims)]
+
+    def resolve_f16(self, op: Operator) -> Optional[List[Tensor]]:
+        print("in resolve_f16")
+        dims = op.attrs['dims']
+        keepdims = op.attrs['keepdims']
+        reduce_type = op.task.attributes['reduce_type']
+        x: Tensor = op.inputs[0]
+        last_dim = x.shape[-1]
+        if x.dtype != dtypes.float16 or last_dim % 2 != 0:
+            return None
+        print("Resolving to Reduce F16")
+        return [reduce_f16(x, dims, keepdims, reduce_type)]
+
+    def resolve_generic(self, op: Operator) -> Optional[List[Tensor]]:
+        print("in resolve_generic")
+        x: Tensor = op.inputs[0]
+        return op.outputs
+
+    def resolve(self, op: Operator) -> Optional[List[Tensor]]:
+        assert isinstance(op, ReduceBaseOp)
+        print("Resolving Reduce")
+        resolve_funcs: List[Callable[[Operator], Any]] = [self.resolve_simplify, self.resolve_f16, self.resolve_generic]
+        for resolve_func in resolve_funcs:
+            outs = resolve_func(op)
+            if outs is not None:
+                return outs
+        return None
