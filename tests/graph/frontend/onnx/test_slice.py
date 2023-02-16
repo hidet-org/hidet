d17ff5c594aca962048228a28b8b6a680b86bfe2
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-02-16 17:19:28 -0500

    add test

---
+++ tests/graph/frontend/onnx/test_slice.py
@@ -0,0 +1,24 @@
+import pytest
+import torch
+
+from utils import check_onnx_and_hidet
+
+
+class SliceModule(torch.nn.Module):
+    def __init__(self, indices):
+        super().__init__()
+        self.indices = indices
+
+    def forward(self, x):
+        return x[self.indices]
+
+
+@pytest.mark.parametrize(
+    'shape,indices', [
+        ((100,), slice(2, None))
+    ]
+)
+def test_slice(shape, indices):
+    check_onnx_and_hidet(SliceModule(indices), [torch.randn(shape)])
+
+
---
+++ tests/graph/frontend/onnx/utils.py
@@ -0,0 +1,50 @@
+from typing import Sequence
+
+import numpy.testing
+import torch
+
+import hidet
+
+
+def check_onnx_and_hidet(
+    torch_model: torch.nn.Module, inputs: Sequence[torch.Tensor], atol=1e-4, rtol=1e-4,
+    device='all'
+):
+    if device == 'all':
+        devices = ['cuda', 'cpu']
+        for dev in devices:
+            check_onnx_and_hidet(torch_model, inputs, atol, rtol, dev)
+        return
+
+    # run torch
+    device = torch.device(device)
+    torch_model = torch_model.to(device).eval()
+    inputs = [x.to(device) for x in inputs]
+    torch_outputs = torch_model(*inputs)
+    if isinstance(torch_outputs, torch.Tensor):
+        torch_outputs = (torch_outputs,)
+
+    # export to onnx
+    onnx_path = hidet.utils.hidet_cache_file('./test_model.onnx')
+    torch.onnx.export(
+        torch_model,
+        args=tuple(inputs),
+        f=onnx_path,
+    )
+
+    # run onnx via hidet
+    onnx_model = hidet.frontend.from_onnx(onnx_path)
+    hidet_inputs = [hidet.from_torch(x) for x in inputs]
+    symbol_inputs = [hidet.symbol_like(x) for x in hidet_inputs]
+    symbol_outputs = onnx_model(*symbol_inputs)
+    graph = hidet.trace_from(symbol_outputs, inputs=symbol_inputs)
+
+    # check outputs
+    hidet_outputs = graph(*hidet_inputs)
+    if isinstance(hidet_outputs, hidet.Tensor):
+        hidet_outputs = (hidet_outputs,)
+    for torch_output, hidet_output in zip(torch_outputs, hidet_outputs):
+        torch_output = torch_output.detach().cpu().numpy()
+        hidet_output = hidet_output.cpu().numpy()
+        numpy.testing.assert_allclose(torch_output, hidet_output, atol=atol, rtol=rtol)
+
