7fcec9f8021a6f40c21e47ae18092d1372d80966
Andre Slavescu <andre.slavescu@gmail.com>
2023-03-23 13:04:58 -0400

    non-linear operators

--- python/hidet/graph/ops/definitions/activation.py
+++ python/hidet/graph/ops/definitions/activation.py
@@ -13,8 +13,9 @@ from typing import Optional
 import math
 from hidet.ir import primitives as prim
 from hidet.ir.expr import if_then_else
-from .utils import Tensor
+from .utils import Tensor, Operator, normalize_dim, input_like
 from .arithmetic import UnaryElementwiseOp, BinaryElementwiseOp
+from .softmax import SoftmaxTask
 
 
 class ReluOp(UnaryElementwiseOp):
@@ -87,6 +88,131 @@ class HardSwishOp(UnaryElementwiseOp):
         )
 
 
+class ThresholdOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, threshold_val: float, value: float) -> Tensor:
+        super().__init__(x, op=lambda v: if_then_else(v > x.dtype(threshold_val), v, x.dtype(value)), name='threshold')
+
+
+class HardTanhOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, min_val: float = -1.0, max_val: float = 1.0) -> Tensor:
+        super().__init__(
+            x,
+            op=lambda v: if_then_else(
+                v > x.dtype(max_val), x.dtype(max_val), if_then_else(v < x.dtype(min_val), x.dtype(min_val), v)
+            ),
+            name='hardtanh',
+        )
+
+
+class EluOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, alpha: float = 1.0) -> Tensor:
+        super().__init__(
+            x, op=lambda v: if_then_else(v > 0, v, x.dtype(alpha) * (prim.exp(v) - x.dtype(1.0))), name='elu'
+        )
+
+
+class SeluOp(UnaryElementwiseOp):
+    def __init__(
+        self,
+        x: Tensor,
+        alpha: float = 1.6732632423543772848170429916717,
+        scale: float = 1.0507009873554804934193349852946,
+    ) -> Tensor:
+        super().__init__(
+            x,
+            op=lambda v: x.dtype(scale)
+            * (prim.max(x.dtype(0.0), v) + prim.min(x.dtype(0.0), x.dtype(alpha) * (prim.exp(v) - x.dtype(-1.0)))),
+            name='selu',
+        )
+
+
+class CeluOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, alpha: float = 1.0) -> Tensor:
+        super().__init__(
+            x,
+            op=lambda v: prim.max(x.dtype(0.0), v)
+            + prim.min(x.dtype(0.0), x.dtype(alpha) * (prim.exp(v / x.dtype(alpha)) - x.dtype(1.0))),
+            name='celu',
+        )
+
+
+class RReluOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, lower: float = 1 / 8, upper: float = 1 / 3) -> Tensor:
+        super().__init__(
+            x, op=lambda v: if_then_else(v >= 0, v, prim.random(x.dtype(lower), x.dtype(upper)) * v), name='rrelu'
+        )
+
+
+class LogSigmoidOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor):
+        super().__init__(x, op=lambda v: prim.log(x.dtype(1.0) / (x.dtype(1.0) + prim.exp(-v))), name='logsigmoid')
+
+
+class HardShrinkOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, lambda_val: float = 0.5):
+        super().__init__(
+            x,
+            op=lambda v: if_then_else(
+                v > x.dtype(lambda_val), v, if_then_else(v < x.dtype(-lambda_val), v, x.dtype(0.0))
+            ),
+            name='hardshrink',
+        )
+
+
+class TanhShrinkOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor):
+        super().__init__(
+            x, op=lambda v: v - (prim.exp(v) - prim.exp(-v)) / (prim.exp(v) + prim.exp(-v)), name='tanhshrink'
+        )
+
+
+class SoftSignOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor):
+        super().__init__(x, op=lambda v: v / (x.dtype(1.0) + prim.abs(v)), name='softsign')
+
+
+class SoftPlusOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, beta: int = 1, threshold_val: int = 20):
+        super().__init__(
+            x,
+            op=lambda v: if_then_else(
+                v * x.dtype(beta) <= x.dtype(threshold_val),
+                (x.dtype(1.0) / x.dtype(beta)) * prim.log(x.dtype(1.0) + prim.exp(x.dtype(beta) * v)),
+                v,
+            ),
+            name='softplus',
+        )
+
+
+class SoftShrinkOp(UnaryElementwiseOp):
+    def __init__(self, x: Tensor, lambda_val: float = 0.5):
+        super().__init__(
+            x,
+            op=lambda v: if_then_else(
+                v > x.dtype(lambda_val),
+                v - x.dtype(lambda_val),
+                if_then_else(v < x.dtype(-lambda_val), v + x.dtype(lambda_val), x.dtype(0.0)),
+            ),
+            name='softshrink',
+        )
+
+
+class SoftmaxOp(Operator):
+    def __init__(self, x: Tensor, axis: int = 1):
+        axis = normalize_dim(axis, len(x.shape))
+        super().__init__(
+            inputs=[x], task=SoftmaxTask(input_like(x, 'x'), axis), attributes={'axis': axis}, name='softmax'
+        )
+
+
+class SoftminOp(Operator):
+    def __init__(self, x: Tensor, axis: int = 1):
+        axis = normalize_dim(axis, len(x.shape))
+        super().__init__(
+            inputs=[-x], task=SoftmaxTask(input_like(x, 'x'), axis), attributes={'axis': axis}, name='softmin'
+        )
+
+
 def relu(x) -> Tensor:
     return ReluOp(x).get_output(0)
 
@@ -125,3 +251,59 @@ def prelu(x: Tensor, slope: Tensor) -> Tensor:
 
 def hardswish(x: Tensor) -> Tensor:
     return HardSwishOp(x).get_output(0)
+
+
+def threshold(x: Tensor, threshold_val: float, value: float) -> Tensor:
+    return ThresholdOp(x, threshold_val, value).get_output(0)
+
+
+def hardtanh(x: Tensor, min_val: float, max_val: float) -> Tensor:
+    return HardTanhOp(x, min_val, max_val).get_output(0)
+
+
+def elu(x: Tensor, alpha: float) -> Tensor:
+    return EluOp(x, alpha).get_output(0)
+
+
+def selu(x: Tensor, alpha: float, scale: float) -> Tensor:
+    return SeluOp(x, alpha, scale).get_output(0)
+
+
+def celu(x: Tensor, alpha: float) -> Tensor:
+    return CeluOp(x, alpha).get_output(0)
+
+
+def rrelu(x: Tensor, lower: float, upper: float) -> Tensor:
+    return RReluOp(x, lower, upper).get_output(0)
+
+
+def logsigmoid(x: Tensor) -> Tensor:
+    return LogSigmoidOp(x).get_output(0)
+
+
+def hardshrink(x: Tensor, lambda_val: float) -> Tensor:
+    return HardShrinkOp(x, lambda_val).get_output(0)
+
+
+def tanhshrink(x: Tensor) -> Tensor:
+    return TanhShrinkOp(x).get_output(0)
+
+
+def softsign(x: Tensor) -> Tensor:
+    return SoftSignOp(x).get_output(0)
+
+
+def softplus(x: Tensor, beta: int, threshold_val: int) -> Tensor:
+    return SoftPlusOp(x, beta, threshold_val).get_output(0)
+
+
+def softshrink(x: Tensor, lambda_val: float) -> Tensor:
+    return SoftShrinkOp(x, lambda_val).get_output(0)
+
+
+def softmax(x: Tensor, axis=1) -> Tensor:
+    return SoftmaxOp(x, axis).get_output(0)
+
+
+def softmin(x: Tensor, axis: int) -> Tensor:
+    return SoftminOp(x, axis).get_output(0)
