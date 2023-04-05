18de9d4d65f87f11e7823c64eaf93f32062bffdf
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-04-04 22:41:44 -0400

    .

--- tests/frontends/torch/test_torch_resnet50.py
+++ tests/frontends/torch/test_torch_resnet50.py
@@ -10,15 +10,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import torch
+import torch.backends.cudnn
 import pytest
 from hidet.testing.torch_utils import check_module
 
 
 @pytest.mark.parametrize('shape', [[1, 3, 224, 224]])
 def test_resnet50(shape):
+    torch.backends.cudnn.allow_tf32 = False  # disable tf32 for accuracy
     model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet50', pretrained=True)
     x = torch.randn(*shape)
     check_module(model, [x], atol=1e-2, rtol=1e-2)
+    torch.backends.cudnn.allow_tf32 = True
 
 
 if __name__ == '__main__':
