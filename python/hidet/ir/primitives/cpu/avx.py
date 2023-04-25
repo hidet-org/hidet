8d0aae4589befe48d501973a02463616641fe08c
BolinSNLHM <bolin.sun@mail.utoronto.ca>
2023-04-25 11:32:26 -0400

    now remember to backup...

---
+++ python/hidet/ir/primitives/cpu/avx.py
@@ -0,0 +1,47 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Union
+
+from hidet.ir.expr import Expr, Call
+from hidet.ir.type import FuncType, VoidType, PointerType
+from hidet.ir.primitives import initialize
+from hidet.ir.primitives.func import register_primitive_function
+from hidet.utils import initialize
+from hidet.ir.primitives.func import call_primitive_func
+
+@initialize()
+def register_primitive_functions():
+    functions = [
+        ('avx_x86_float32x4_broadcast', '_mm_broadcast_ss', FuncType([PointerType('float32')], 'float32x4')),
+        ('avx_x86_float32x4_fmadd', '_mm_fmadd_ps', FuncType(['float32x4', 'float32x4', 'float32x4'], 'float32x4')),
+        ('avx_x86_float32x4_load', '_mm_load_ps', FuncType([PointerType('float32')], 'float32x4')),
+        ('avx_x86_float32x4_store', '_mm_store_ps', FuncType([PointerType('float32'), 'float32x4'], VoidType()))
+    ]
+    for name, codegen_name, func_type in functions:
+        register_primitive_function(name=name, func_or_type=func_type, codegen_name=codegen_name)
+
+
+def avx_f32x4_broadcast(addr: Expr) -> Call:
+    return call_primitive_func('avx_x86_float32x4_broadcast', [addr])
+
+
+def avx_f32x4_fmadd(a: Expr, b: Expr, c: Expr) -> Call:
+    return call_primitive_func('avx_x86_float32x4_fmadd', [a, b, c])
+
+
+def avx_f32x4_load(addr: Expr) -> Call:
+    return call_primitive_func('avx_x86_float32x4_laod', [addr])
+
+
+def avx_f32x4_store(addr: Expr, src: Expr) -> Call:
+    return call_primitive_func('avx_x86_float32x4_store', [addr, src])
+
