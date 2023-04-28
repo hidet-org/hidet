81eb7a2bef20fff2e4f6439d6e74259aed1b8f68
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-04-27 20:26:55 -0400

    [Stack] Allow the the ulimit stack size less than expected (#195)
    * .
    
    * .

--- python/hidet/utils/stack_limit.py
+++ python/hidet/utils/stack_limit.py
@@ -12,11 +12,23 @@
 """
 Allow more stack space and recursion depth for python.
 """
+from typing import Tuple
+import warnings
 import sys
 import resource
 
-# allow up to 128MB stack space
-resource.setrlimit(resource.RLIMIT_STACK, (2**29, -1))
+# allow up to 512 MiB stack space
+expected_stack_size = 2**29  # 512 MiB
+stack_limit: Tuple[int, int] = resource.getrlimit(resource.RLIMIT_STACK)
+if stack_limit[1] != resource.RLIM_INFINITY and stack_limit[1] < expected_stack_size:
+    warnings.warn(
+        f'The hard limit for stack size is too small ({stack_limit[1] / 2**20:.1f} MiB), '
+        f'we recommend to increase it to {expected_stack_size / 2**20:.1f} MiB. '
+        'If you are the root user on Linux OS, you could refer to `man limits.conf` to increase this limit.'
+    )
+    resource.setrlimit(resource.RLIMIT_STACK, (stack_limit[1], stack_limit[1]))
+else:
+    resource.setrlimit(resource.RLIMIT_STACK, (expected_stack_size, stack_limit[1]))
 
 # allow up to 10^5 recursive python calls, increase this when needed
 sys.setrecursionlimit(100000)
