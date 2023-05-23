77c2cd0517bba0d28e938cb7e37798fe6d6f3b48
Allan Lin <allan.lin@centml.ai>
2023-05-23 16:00:41 -0400

    [Graph] Remove unnecessary ops

--- python/hidet/graph/ops/schedules/__init__.py
+++ python/hidet/graph/ops/schedules/__init__.py
@@ -12,7 +12,4 @@
 from . import cpu
 from . import cuda
 
-from .cuda.softmax import softmax_cuda_schedule
-from .cuda.reduce import cuda_schedule_reduce_by_default, cuda_schedule_reduce_by_warp_reduce
-
 from .common import Schedule
