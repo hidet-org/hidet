212fef8c0c6e747d09ed585a80cec4ba810b31f5
Xin Li <xin@centml.ai>
2023-05-23 13:32:18 -0400

    update param names to address review feedbacks

--- python/hidet/option.py
+++ python/hidet/option.py
@@ -103,7 +103,8 @@ def register_hidet_options():
         name='parallel_tune',
         type_hint='int, float',
         default_value=(-1, 1.5),
-        description='The number of parallel jobs and memory reserved for each job',
+        description='The pair (max_parallel_jobs, mem_gb_per_job) that describe '
+        'the maximum number of parallel jobs and memory reserved for each job',
     )
     register_option(
         name='save_lower_ir',
@@ -482,18 +483,20 @@ def get_parallel_build() -> bool:
     return OptionContext.current().get_option('parallel_build')
 
 
-def parallel_tune(num_jobs: int = -1, mem_gb_per_job: float = 1.5):
+def parallel_tune(max_parallel_jobs: int = -1, mem_gb_per_job: float = 1.5):
     """
-    Whether to build operators in parallel.
+    Specify the maximum number of parallel compilation jobs to do,
+    and the number of GiB preserved for each job.
 
     Parameters
     ----------
-    num_jobs: int
-        The maximum number of parallel jobs allowed, default -1 (decided by the system)
+    max_parallel_jobs: int
+        The maximum number of parallel jobs allowed, default -1
+        (the number of available vcpu returned by `os.cpu_count()`).
     mem_gb_per_job: float
         The minimum amount of memory (in GiB) reserved for each tuning job, default 1.5GiB.
     """
-    OptionContext.current().set_option('parallel_tune', (num_jobs, mem_gb_per_job))
+    OptionContext.current().set_option('parallel_tune', (max_parallel_jobs, mem_gb_per_job))
 
 
 def get_parallel_tune() -> Tuple[int, float]:
