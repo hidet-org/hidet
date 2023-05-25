c342a9c3955f71120b2a759d3fa12a9aab5ddf98
Xin Li <xin@centml.ai>
2023-05-25 01:43:10 -0400

    WIP

--- python/hidet/graph/ops/definitions/normalize/norm.py
+++ python/hidet/graph/ops/definitions/normalize/norm.py
@@ -105,48 +105,55 @@ class NormalizeTask(Task):
         return False
 
     def allow_epilogue(self) -> bool:
-        return False
+        return True
 
     def implement_cuda(self, working_dir: str) -> IRModule:
         import hidet
-        row_major = DataLayout.row_major
-        warp_size = 32
-        block_size = warp_size
         x, y = self.inputs[0], self.outputs[0]
         shape: List[int] = list(x.const_shape)
         dims = self.dims
-        if self.keep_dim:
-            remain_shape = [v if i not in dims else 1 for i, v in enumerate(shape)]
-        else:
-            remain_shape = [v for i, v in enumerate(shape) if i not in dims]
+        spaitial_shape = [v for i, v in enumerate(shape) if i not in dims]
         reduce_shape = [shape[i] for i in dims]
-        reduce_extent = prod(reduce_shape)
-        shape_32bit = [s // 2 if i == len(shape) - 1 else s for i, s in enumerate(shape)]
-        remain_layout = spatial(*remain_shape)
-        x_f32_layout = row_major(shape_32bit)
 
-        spatial_shape = []
-        repeat_shape = []
-        for i in range(len(shape_32bit)):
-            if i == len(shape_32bit) - 1:
-                spatial_shape.append(warp_size)
-                repeat_shape.append((shape_32bit[i] + warp_size - 1) // warp_size)  # num warps per row
-            elif i in dims:
-                spatial_shape.append(1)
-                repeat_shape.append(shape_32bit[i])
-            else:
-                spatial_shape.append(shape_32bit[i])
-                repeat_shape.append(1)
+        spatial_extent = prod(spatial_shape)
+        reduce_extent = prod(reduce_shape)
+        row_major = DataLayout.row_major
+
+        warp_size = 32
+        # at least warp_size, at most 1024
+        block_size = min(max(warp_size, reduce_extent), 1024)
+
+        spatial_layout = spatial(*remain_shape)
+        reduction_layout = row_major(reduce_shape)
+
+        # start with block level assignment
+        spatial_layout = spatial(*(spatial_shape + ([1] * len(reduce_shape))))
+
+        # We can only reduce 1024 numbers in a threadblock at once
+        repeat_reduction = math.ceil(reduce_extent  / block_size)
+        reduction_layout = repeat(repeat_reduction, 1) * spatial(block_size)
+
+
         task_layout = repeat(*repeat_shape) * spatial(*spatial_shape)
-        grid_size = remain_layout.num_workers
+        grid_size = spatial_layout.num_workers
         accumulate_dtype = self.attrs['accumulate_dtype']
-        reduce_type = self.attrs['reduce_type']
-        ro = ReduceOperation.from_name(reduce_type)
+
+        dtype = self.inputs[0].type.dtype
+        used_smem_bytes_per_block = match.ceil(block_size / warp_size)  * dtype.nbytes
+        smem_mean = tensor('shared', 'int8', shape=[used_smem_bytes_per_block])
+        smem_var = tensor('shared', 'int8', shape=[used_smem_bytes_per_block])
 
         with hidet.script_module() as module:
 
             @hidet.script
-            def reduce_kernel(x: f16[x.const_shape], y: f16[y.const_shape]):
+            def welford_combine(mean_a, m2_a, count_a, mean_b, m2_b, count_b):
+                count = count_a + count_b
+                delta = mean_b - mean_a
+                mean = mean_a + delta * count_b / count
+                m2 = m2_a + m2_b + delta * delta * count_a * count_b / count
+
+            @hidet.script
+            def norm_kernel(x: f16[x.const_shape], y: f16[y.const_shape]):
                 attr.cuda_grid_dim = grid_size
                 attr.cuda_block_dim = block_size
                 attr.cuda_min_blocks = 1
@@ -155,15 +162,16 @@ class NormalizeTask(Task):
                 x_f32 = x
 
                 reg32 = register_tensor(f32, [1])
-                regs16 = tensor_pointer('float16', shape=[2])
-                regs16 = reg32
-                rv = register_tensor(accumulate_dtype, [1])
-                rv[0] = ro.initial_value(data_type(accumulate_dtype))
+                mean = register_tensor(accumulate_dtype, [1])
+                m2 = register_tensor(accumulate_dtype, [1])
+                mean = data_type(accumulate_dtype).zero
+                m2 = data_type(accumulate_dtype).zero
+                count = data_type('int32').zero
+
                 for indices in task_layout.on(threadIdx.x + blockIdx.x * block_size):
                     if x_f32_layout.within_bound(indices):
                         reg32[0] = x_f32.read(indices, protected=False)
-                        rv[0] = ro.combine(rv[0], regs16[0])
-                        rv[0] = ro.combine(rv[0], regs16[1])
+
                 # Warp reduce by shuffle down
                 mask = active_mask()
                 rv[0] = ro.combine(rv[0], shfl_down_sync(mask, rv[0], 16, 32))
