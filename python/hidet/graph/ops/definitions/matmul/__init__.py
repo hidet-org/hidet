fcbb094b44cd1a635d86751b6f30c48f080bd9c7
Bolin Sun <bolin.sun@mail.utoronto.ca>
2023-05-26 23:02:50 -0400

    ..

--- python/hidet/graph/ops/definitions/matmul/__init__.py
+++ python/hidet/graph/ops/definitions/matmul/__init__.py
@@ -14,7 +14,5 @@ from .batch_matmul import batch_matmul, BatchMatmulOp, BatchMatmulTask
 from . import resolve
 
 from .matmul_f32_x86 import matmul_x86
-from .matmul_f32_x86_v2 import matmul_x86_onednn
 
 from .matmul_f32_x86 import MatmulF32Taskx86, Matmulx86Op
-from .matmul_f32_x86_v2 import MatmulF32Taskx86OneDNN, MatmulX86OneDNNOp
