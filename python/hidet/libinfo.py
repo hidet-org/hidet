2bc16802631d06766b2d29a42de2c1c735aafb7f
Qidong <soodoshll@gmail.com>
2023-06-17 23:27:33 -0400

    clean

--- python/hidet/libinfo.py
+++ python/hidet/libinfo.py
@@ -67,12 +67,3 @@ def get_library_search_dirs() -> List[str]:
     ]
     return [os.path.abspath(os.path.join(root, relative)) for relative in relative_dirs]
 
-def _get_nccl_dirs():
-    import site
-    return [os.path.join(root, 'nvidia', 'nccl') for root in site.getsitepackages()]
-
-def get_nccl_include_dirs():
-    return [os.path.join(root, 'include') for root in _get_nccl_dirs()]
-
-def get_nccl_library_search_dirs():
-    return [os.path.join(root, 'lib') for root in _get_nccl_dirs()]
