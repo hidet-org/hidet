bb4d6d1ff0433455184ee7fe430ef8f39567e9a5
Qidong <soodoshll@gmail.com>
2023-06-27 15:39:04 -0400

    format

--- python/hidet/distributed/group.py
+++ python/hidet/distributed/group.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#pylint: disable=W0223
+# pylint: disable=W0223
 
 from typing import Optional, List
 
