b4f1d35677faa9057a4676baef6f490a788c4c21
AL <allanlin666666@gmail.com>
2023-07-03 21:20:05 -0400

    refactor changes

--- python/hidet/graph/ops/quant/symmetric.py
+++ python/hidet/graph/ops/quant/symmetric.py
@@ -9,11 +9,9 @@ from hidet.ir.compute import cops
 from hidet.graph.ops.utils import Task, Operator, Tensor, input_like, normalize_dim
 
 
-# pylint: disable=dangerous-default-value
 class SymmetricQuantizationTask(Task):
     def __init__(self, w: TensorNode, quant_type: DataType, dims: Union[int, List[int]] = -1):
         dims = normalize_dim(dims, len(w.shape))
-        self._assert(all(i >= 0 or i < len(w.shape) for i in dims), "dims are out of bounds")
 
         wm = compute(
             name='abs', shape=w.shape, fcompute=lambda *indices: if_then_else(w[indices] >= 0, w[indices], -w[indices])
@@ -37,9 +35,8 @@ class SymmetricQuantizationTask(Task):
 
 
 class SymmetricDeQuantizationTask(Task):
-    def __init__(self, wq: TensorNode, scale: TensorNode, dims=[-1]):
-        dims = [i if i >= 0 else len(wq.shape) + i for i in dims]
-        self._assert(all(i >= 0 or i < len(wq.shape) for i in dims), "dims are out of bounds")
+    def __init__(self, wq: TensorNode, scale: TensorNode, dims: Union[int, List[int]] = -1):
+        dims = normalize_dim(dims, len(wq.shape))
 
         def unscale_weight(*indices):
             scale_indices = [indices[i] for i in range(len(indices)) if not i in dims]
@@ -50,7 +47,7 @@ class SymmetricDeQuantizationTask(Task):
 
 
 class SymmetricQuantizationOp(Operator):
-    def __init__(self, w: Tensor, quant_type: ir.type.DataType, dims=[-1]):
+    def __init__(self, w: Tensor, quant_type: ir.type.DataType, dims: Union[int, List[int]] = -1):
         super().__init__(
             inputs=[w],
             attributes={'dims': dims, 'quant_type': quant_type},
@@ -59,7 +56,7 @@ class SymmetricQuantizationOp(Operator):
 
 
 class SymmetricDeQuantizationOp(Operator):
-    def __init__(self, wq: Tensor, scale: Tensor, dims=[-1]):
+    def __init__(self, wq: Tensor, scale: Tensor, dims: Union[int, List[int]] = -1):
         super().__init__(
             inputs=[wq, scale],
             attributes={'dims': dims},
@@ -67,10 +64,10 @@ class SymmetricDeQuantizationOp(Operator):
         )
 
 
-def symmetric_quantize(w: Tensor, quant_type: Union[str, ir.type.DataType] = 'int8', dims=[-1]):
+def symmetric_quantize(w: Tensor, quant_type: Union[str, ir.type.DataType] = 'int8', dims: Union[int, List[int]] = -1):
     op = SymmetricQuantizationOp(w, ir.type.data_type(quant_type), dims=dims)
     return op.outputs[0], op.outputs[1]
 
 
-def symmetric_dequantize(wq: Tensor, scale: Tensor, dims=[-1]):
+def symmetric_dequantize(wq: Tensor, scale: Tensor, dims: Union[int, List[int]] = -1):
     return SymmetricDeQuantizationOp(wq, scale, dims).outputs[0]
