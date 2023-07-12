cdc55f0825b9c1977057fc201a0f15663bc35517
hjjq <hanjie.qiu@mail.utoronto.ca>
2023-07-12 18:26:42 -0400

    .

--- scripts/regression/model_performance.py
+++ scripts/regression/model_performance.py
@@ -1,6 +1,18 @@
 import torch
 import hidet
 
+def bert_regression():
+    pass
+
+def resnet_regression():
+    pass
+
+def efficientnet_regression():
+    pass
+
+def llama_regression():
+    pass
+
 def model_performance_regression(report_file):
     print(report_file)
     pass
--- scripts/regression/op_performance.py
+++ scripts/regression/op_performance.py
@@ -1,6 +1,27 @@
 import torch
 import hidet
 
+# [M, N, K] where C_mxn = A_mxk @ B_kxn
+matmul_shapes = [
+    []
+]
+def batch_matmul_regression():
+
+    pass
+
+def matmul_f16_regression():
+    pass
+
+def fmha_regression():
+    pass
+
+def conv2d_regression():
+    pass
+
+def reduce_regression():
+    pass
+
+
 def op_performance_regression(report_file):
     print(report_file)
     pass
