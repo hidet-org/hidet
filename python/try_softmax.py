ac40695ec9d6545a12e5fe018856d4601b2c6de5
Kevin Qu <qukevin99@gmail.com>
2023-08-04 21:56:22 -0400

    softmax works

--- python/try_softmax.py
+++ python/try_softmax.py
@@ -4,8 +4,9 @@ import torch
 import hidet
 from hidet.graph.ops import softmax
 import torch.nn as nn
-shapes = [([8, 1000], -1), ([32, 512, 512], -1), ([32, 512, 512], 1), ([8, 3, 224, 224], -1),
-          ([32, 128, 768], 1)]
+shapes = [([1, 2, 3], 1), ([8, 8, 8, 8], 0), ([8, 8, 8, 8], 1), ([8, 8, 8, 8], 2), ([8, 8, 8, 8], 3), ([2, 2, 8], 0),
+          ([1, 2, 16], 1), ([8, 8, 8], 1), ([8, 1000], -1), ([32, 512, 512], -1), ([32, 512, 512], 1),
+          ([8, 3, 224, 224], -1), ([32, 128, 768], 1)]
 # shapes = [([4, 100], -1)]
 hidet.option.search_space(0)
 # hidet.option.runtime_check(False)
@@ -22,7 +23,7 @@ for shape, axis in shapes:
     device = torch.device("cpu")
     m = nn.Softmax(dim=axis)
     a_torch = torch.from_numpy(np.array(a.numpy(), copy=True, dtype='float32'))
-
+    print(a)
     np.testing.assert_allclose(b.numpy(), m(a_torch), rtol=1e-05, atol=1e-08)
     print("hidet and pytorch tensors match")
 
@@ -34,7 +35,7 @@ for shape, axis in shapes:
     hidet_latency = hidet.utils.benchmark_func(lambda: compiled_func(a, b), warmup=10, repeat=50)
     pt_latency = hidet.utils.benchmark_func(lambda: m(a_torch), warmup=10, repeat=50)
     np_latency = hidet.utils.benchmark_func(lambda: numpy_softmax(a.numpy(), axis_=axis), warmup=10, repeat=50)
-    print("for shape of", shape, ":", "hidet:", hidet_latency, "pytorch:", pt_latency, "numpy:", np_latency)
+    print("shape", shape, "and axis", axis, "hidet:", hidet_latency, "pytorch:", pt_latency, "numpy:", np_latency)
     print("fastest is:", ["hidet", "pytorch", "numpy"][np.argmin([hidet_latency, pt_latency, np_latency])])
     # print(b, m(a_torch))
 # softmax([bs, 1000], axis=1)  # bs = 1, 2, 4, 8
