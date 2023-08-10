b4cb80ee04ef0f5d25ea8b620c30c905238be8b1
AL <allanlin666666@gmail.com>
2023-08-10 09:49:49 -0400

    .

--- python/hidet/ir/lark_test.py
+++ python/hidet/ir/lark_test.py
@@ -12,3 +12,16 @@ with open('test.txt') as f:
     tree = json_parser.parse(f.read())
     print(tree.pretty())
 
+# %%
+
+grammar = """
+    expr: expr "+" expr | SIGNED_NUMBER
+    %import common.SIGNED_NUMBER
+    %import common.WS
+    %ignore WS
+"""
+
+parser = Lark(grammar, start="expr")
+
+tree = parser.parse("42 + 3 + 4")
+print(tree.pretty())
