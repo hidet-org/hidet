805959e893921f95b6c9861f565475b1c811ce76
BolinSNLHM <bolin.sun@mail.utoronto.ca>
2023-08-29 05:48:19 -0400

    ...

--- python/mat_new.py
+++ python/mat_new.py
@@ -30,7 +30,7 @@ hidet.option.cache_dir("./wtf")
 target = tvm.target.Target("llvm -mcpu=core-avx2")
 debug_cache_tuning(True)
 hidet.option.search_space(0)
-for m, n, k in [(64, 64, 64)]:
+for m, n, k in [(64, 64, 64), (444, 3072, 768), (768, 768, 768), (123, 456, 789)]:
     a = hidet.randn([m, k], device='cpu')
     b = hidet.randn([k, n], device='cpu')
     x1 = hidet.symbol_like(a)
@@ -46,11 +46,6 @@ for m, n, k in [(64, 64, 64)]:
     actual = c.numpy()
     desired = a.numpy() @ b.numpy()
 
-    for i in range(m):
-        for j in range(n):
-            if abs(actual[i, j] - desired[i, j]) < 1e-3:
-                print(f"actually match at {i}, {j}")
-
 
     np.testing.assert_allclose(
         actual=actual,
@@ -61,47 +56,47 @@ for m, n, k in [(64, 64, 64)]:
 
     print("passed for m={}, n={}, k={}".format(m, n, k))
 
-    hidet_latency = hidet.utils.benchmark_func(
-        lambda: compiled_func(a, b), repeat=50
-    )
-    np_latency = hidet.utils.benchmark_func(
-        lambda: a.numpy() @ b.numpy(), repeat=50
-    )
-
-    ansor_task = tvm.auto_scheduler.SearchTask(
-        func=matmul_ansor, args=(m, k, n, "float32"), target=target
-    )
-    log_file = f"matmul_{m}x{k}x{n}.json"
-    tune_option = auto_scheduler.TuningOptions(
-        num_measure_trials=1000,
-        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
-        verbose=2,
-    )
-
-    ansor_task.tune(tune_option)
-    sch, args = ansor_task.apply_best(log_file)
-    with open(f"./matmul_TIR_{m}x{k}x{n}", 'w') as f:
-        f.write(str(tvm.lower(sch, args, simple_mode=True)))
-        ansor_func = tvm.build(sch, args, target)
-        dev = tvm.cpu()
-        a_tvm = tvm.nd.array(a.numpy(), device=dev)
-        b_tvm = tvm.nd.array(b.numpy(), device=dev)
-        c_tvm = tvm.nd.empty((m, n), device=dev)
-
-        ansor_func(a_tvm, b_tvm, c_tvm)
-
-        np.testing.assert_allclose(
-            actual=c_tvm.numpy(),
-            desired=a_tvm.numpy() @ b_tvm.numpy(),
-            rtol=1e-3,
-            atol=1e-3
-        )
-
-        ansor_latency = hidet.utils.benchmark_func(
-            lambda: ansor_func(a_tvm, b_tvm, c_tvm), repeat=30
-        )
-
-        with open(f"./perf_{m}x{k}x{n}.txt", 'w') as f:
-            f.write(f"m={m}, k={k}, n={n}: hidet takes {hidet_latency:.2f} ms\n")
-            f.write(f"m={m}, k={k}, n={n}: numpy takes {np_latency: .2f} ms\n")
-            f.write(f"m={m}, k={k}, n={n}: ansor takes {ansor_latency: .2f} ms\n")
+    # hidet_latency = hidet.utils.benchmark_func(
+    #     lambda: compiled_func(a, b), repeat=50
+    # )
+    # np_latency = hidet.utils.benchmark_func(
+    #     lambda: a.numpy() @ b.numpy(), repeat=50
+    # )
+    #
+    # ansor_task = tvm.auto_scheduler.SearchTask(
+    #     func=matmul_ansor, args=(m, k, n, "float32"), target=target
+    # )
+    # log_file = f"matmul_{m}x{k}x{n}.json"
+    # tune_option = auto_scheduler.TuningOptions(
+    #     num_measure_trials=1000,
+    #     measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+    #     verbose=2,
+    # )
+    #
+    # ansor_task.tune(tune_option)
+    # sch, args = ansor_task.apply_best(log_file)
+    # with open(f"./matmul_TIR_{m}x{k}x{n}", 'w') as f:
+    #     f.write(str(tvm.lower(sch, args, simple_mode=True)))
+    #     ansor_func = tvm.build(sch, args, target)
+    #     dev = tvm.cpu()
+    #     a_tvm = tvm.nd.array(a.numpy(), device=dev)
+    #     b_tvm = tvm.nd.array(b.numpy(), device=dev)
+    #     c_tvm = tvm.nd.empty((m, n), device=dev)
+    #
+    #     ansor_func(a_tvm, b_tvm, c_tvm)
+    #
+    #     np.testing.assert_allclose(
+    #         actual=c_tvm.numpy(),
+    #         desired=a_tvm.numpy() @ b_tvm.numpy(),
+    #         rtol=1e-3,
+    #         atol=1e-3
+    #     )
+    #
+    #     ansor_latency = hidet.utils.benchmark_func(
+    #         lambda: ansor_func(a_tvm, b_tvm, c_tvm), repeat=30
+    #     )
+    #
+    #     with open(f"./perf_{m}x{k}x{n}.txt", 'w') as f:
+    #         f.write(f"m={m}, k={k}, n={n}: hidet takes {hidet_latency:.2f} ms\n")
+    #         f.write(f"m={m}, k={k}, n={n}: numpy takes {np_latency: .2f} ms\n")
+    #         f.write(f"m={m}, k={k}, n={n}: ansor takes {ansor_latency: .2f} ms\n")
