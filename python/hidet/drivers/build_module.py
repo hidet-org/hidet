hidet-org/hidet

commit 4d71a8d9bb5e4c8af1c8c7d5efd6ea35193714e8

[Operator] Support advanced options for pooling operators (#399)
1. Support count_include_pad and divisor_override in average pool.
2. Refactor avg and max pool operators computation definition

python/hidet/ir/schedulers/base.py
@@ -10,6 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Union, List, Dict, Sequence, Tuple, Set, Optional
+from collections import defaultdict
 
 from hidet.ir.type import DataType, tensor_pointer_type
 from hidet.ir.expr import TensorElement, Expr, Var, SymbolVar, Constant, scalar_var, convert, cast
@@ -18,7 +19,7 @@ from hidet.ir.task import Task
 from hidet.ir.func import Function
 from hidet.ir.module import IRModule
 from hidet.ir.builders import FunctionBuilder, StmtBuilder
-from hidet.ir.functors import ExprRewriter, ExprVisitor, ComputeVisitor, ComputeRewriter, TypeRewriter
+from hidet.ir.functors import ExprRewriter, ExprVisitor, ComputeVisitor, ComputeRewriter, TypeRewriter, IRVisitor
 from hidet.ir.tools import IRPrinter, collect, rewrite, infer_type, simplify, collect_free_vars
 from hidet.ir.compute import ScalarInput, TensorInput, GridCompute, ReduceCompute, ArgReduceCompute
 from hidet.ir.compute import TensorNode, ScalarNode
@@ -30,6 +31,26 @@ from hidet.utils.namer import Namer
 
 class ScalarComputeFound(Exception):
     pass
+
+
+class UsageCounter(IRVisitor):
+    def __init__(self):
+        super().__init__()
+        self.usage: Dict[Union[TensorNode, ScalarNode], int] = defaultdict(int)
+
+    def visit_TensorInput(self, node: TensorInput):
+        self.usage[node] += 1
+
+    def visit_ScalarInput(self, node: ScalarInput):
+        self.usage[node] += 1
+
+    def visit_GridCompute(self, node: GridCompute):
+        self.usage[node] += 1
+        super().visit_GridCompute(node)
+
+    def visit_ReduceCompute(self, node: ReduceCompute):
+        self.usage[node] += 1
+        super().visit_ReduceCompute(node)
 
 
 class GridComputeInlineChecker(ExprVisitor, ComputeVisitor):
@@ -75,8 +96,9 @@ def can_inline_grid_compute(gc: GridCompute) -> bool:
 
 
 class GridComputeInliner(ExprRewriter, ComputeRewriter):
-    def __init__(self):
+    def __init__(self, usage_count: Dict[Union[TensorNode, ScalarNode], int]):
         super().__init__()
+        self.usage_count: Dict[Union[TensorNode, ScalarNode], int] = usage_count
 
     def inline(self, node: TensorNode):
         return self.visit(node)
@@ -88,7 +110,8 @@ class GridComputeInliner(ExprRewriter, ComputeRewriter):
         base = self(e.base)
         indices = [self(index) for index in e.indices]
         if isinstance(base, GridCompute):
-            if can_inline_grid_compute(base):
+            assert isinstance(e.base, TensorNode)
+            if can_inline_grid_compute(base) or self.usage_count[e.base] == 1:
                 return rewrite(base.value, {axis: index for axis, index in zip(base.axes, indices)})
         return ExprRewriter.visit_TensorElement(self, e)
 
@@ -120,7 +143,11 @@ def inline_grid_compute(nodes: List[TensorNode]) -> List[TensorNode]:
     ret: List[TensorNode]
         The nodes after inlining.
     """
-    inliner = GridComputeInliner()
+    usage_counter = UsageCounter()
+    usage_counter.visit(nodes)
+
+    inliner = GridComputeInliner(usage_counter.usage)
+
     return [inliner.inline(node) for node in nodes]
 
 

tests/frontends/torch/models/test_torch_densenet121.py
@@ -10,18 +10,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import torch
-import torch.backends.cudnn
 import pytest
 from hidet.testing.torch_utils import check_module
 
 
 @pytest.mark.parametrize('shape', [[1, 3, 224, 224]])
-@pytest.mark.parametrize('dynamic', [False, True])
-@pytest.mark.parametrize('dtype, tol', [(torch.float16, 2e-2), (torch.float32, 1e-4)])
-def test_resnet50(shape, dynamic, dtype, tol):
-    model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet50', pretrained=True).cuda().eval().to(dtype)
-    x = torch.randn(*shape).cuda().to(dtype) * 0.1796 + 0.5491
-    check_module(model, [x], atol=tol, rtol=tol, dynamic=dynamic)
+def test_densenet121(shape):
+    model = torch.hub.load('pytorch/vision:v0.6.0', 'densenet121', pretrained=True).cuda().eval().to(torch.float16)
+    x = torch.randn(*shape).cuda().to(torch.float16) * 0.1796 + 0.5491
+    check_module(model, [x], atol=2e-2, rtol=2e-2, dynamic=False)
 
 
 if __name__ == '__main__':

python/hidet/graph/frontend/torch/register_functions.py
@@ -328,27 +328,23 @@ def unsqueeze(x: Tensor, dim: int):
 def avg_pool2d(
     x: Tensor, kernel_size, stride=None, padding=0, ceil_mode=False, count_include_pad=True, divisor_override=None
 ):
-    if ceil_mode:
-        raise NotImplementedError("ceil_mode=True")
-    if not count_include_pad:
-        raise NotImplementedError("count_include_pad=False")
-    if divisor_override is not None:
-        raise NotImplementedError("divisor_override is not None")
     if stride is None:
         stride = kernel_size
-    y = ops.avg_pool2d(x, kernel_size, stride, padding)
+    y = ops.avg_pool2d(
+        x,
+        kernel_size,
+        stride,
+        padding,
+        ceil_mode=ceil_mode,
+        count_include_pad=count_include_pad,
+        divisor_override=divisor_override,
+    )
     return y
 
 
 @register_function(torch.nn.functional.avg_pool3d)
 def avg_pool3d(x: Tensor, kernel_size, stride, padding, ceil_mode=False, count_include_pad=True, divisor_override=None):
-    if ceil_mode:
-        raise NotImplementedError("ceil_mode=True")
-    if not count_include_pad:
-        raise NotImplementedError("count_include_pad=False")
-    if divisor_override is not None:
-        raise NotImplementedError("divisor_override is not None")
-    y = ops.avg_pool3d(x, kernel_size, stride, padding)
+    y = ops.avg_pool3d(x, kernel_size, stride, padding, ceil_mode, count_include_pad, divisor_override)
     return y
 
 
@@ -1238,7 +1234,7 @@ def isinf(x: Tensor) -> Tensor:
 
 
 @register_function(torch.nn.functional.pad)
-def torch_pad(x: Tensor, pad: Union[Tuple[int], List[int]], mode: str = 'constant', value=0):
+def torch_pad(x: Tensor, pad: Union[Tuple[int, ...], List[int]], mode: str = 'constant', value=0):
     if isinstance(pad, tuple):
         pad = list(pad)
     # Torch's pad list has form [p2left, p2right, p1left, p1right, p0left, p0right]

python/hidet/graph/frontend/torch/register_modules.py
@@ -162,6 +162,13 @@ class HidetMaxPool3d(HidetModule):
         )
 
 
+@register_module(torch.nn.ZeroPad2d)
+class HidetZeroPad2d(HidetModule):
+    def __call__(self, x: Tensor) -> Tensor:
+        assert isinstance(self.mod, torch.nn.ZeroPad2d)
+        return regs.torch_pad(x=x, pad=self.mod.padding, mode='constant', value=0.0)
+
+
 @register_module(torch.nn.Linear)
 class HidetLinear(HidetModule):
     def __init__(self, torch_module: torch.nn.Module):

python/hidet/graph/ops/conv2d/conv2d_gemm.py
@@ -352,11 +352,11 @@ class Conv2dGemmFp16Task(Task):
             f"expected groups * WC == C, got groups: {groups}, WC: {WC}, C: {C}; make sure the image is channels last!",
         )
         self._assert(
-            DILX > 0 and DILY > 0 and STRX > 0 and STRY > 0,
+            logical_and(DILX > 0, DILY > 0, STRX > 0, STRY > 0),
             f"dilations and strides must be larger than 0, got strides={(STRY, STRX)}, dilations={(DILY, DILX)}",
         )
         self._assert(parallel_k_parts > 0, "expected parallel_k_parts to be greater than 0")
-        self._assert(H >= KY and W >= KX, "expected image dimensions to be greater than filter dimensions")
+        self._assert(logical_and(H >= KY, W >= KX), "expected image dimensions to be greater than filter dimensions")
 
         OUT_H = (H - DILY * (KY - 1) - 1) // STRY + 1
         OUT_W = (W - DILX * (KX - 1) - 1) // STRX + 1

python/hidet/graph/transforms/subgraph_rewrite.py
@@ -12,6 +12,7 @@
 # pylint: disable=unused-import
 from typing import List, Optional, Dict, Tuple, Set
 import logging
+import warnings
 
 from hidet.graph.flow_graph import FlowGraph, Operator, Tensor
 from hidet.graph.transforms import GraphPass, PassContext
@@ -53,7 +54,7 @@ class SubgraphRewritePass(GraphPass):
             if not updated:
                 graph.update_nodes()
                 return graph
-        print('Exceeded maximum number of transforms {}, stop early.'.format(self.max_num_transforms))
+        warnings.warn('Exceeded maximum number of sub-graph transforms {}, stop early.'.format(self.max_num_transforms))
         graph.update_nodes()
         return graph
 

python/hidet/testing/torch_utils.py
@@ -12,6 +12,7 @@
 from typing import Sequence
 import numpy.testing
 import torch
+import torch.backends.cudnn
 from torch import nn
 
 
@@ -30,7 +31,11 @@ def check_module(model: torch.nn.Module, args: Sequence[torch.Tensor], atol=1e-4
     args = [x.cuda() if isinstance(x, torch.Tensor) else x for x in args]
     # we use a lambda to make sure the model is compiled by pytorch
     model_opt = torch.compile(lambda *args, **kwargs: model(*args, **kwargs), backend='hidet', dynamic=dynamic)
+
+    torch.backends.cudnn.allow_tf32 = False  # disable tf32 for accuracy
     torch_outputs = model(*args)
+    torch.backends.cudnn.allow_tf32 = True
+
     hidet_outputs = model_opt(*args)
     if isinstance(torch_outputs, torch.Tensor):
         torch_outputs = (torch_outputs,)

commit 7c71965a220e2696dba123d060cd4ad049405563

[Fixbug] Fix a performance bug in auto-scheduler (#402)
Introduced in #399

python/hidet/drivers/build_module.py
@@ -26,6 +26,7 @@ from hidet.ir.type import FuncType
 from hidet.ir.target import Target
 from hidet.transforms import lower, PassContext, SaveIRInstrument, ProfileInstrument
 from hidet.utils.multiprocess import parallel_imap
+from hidet.utils.stack_limit import set_stack_limit
 
 logger = logging.Logger(__name__)
 logger.setLevel(logging.INFO)
@@ -99,6 +100,10 @@ def build_ir_module(ir_module: IRModule, output_dir: str, *, target: str, output
         src_path = os.path.join(output_dir, 'source.cc')
     else:
         raise ValueError(f'Invalid target: {target}')
+
+    # set the recursion limit before every lowering, because some other packages might change this value to a lower
+    # value that we need
+    set_stack_limit()
 
     # lower ir module
     instruments = []

python/hidet/ir/schedulers/base.py
@@ -10,7 +10,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Union, List, Dict, Sequence, Tuple, Set, Optional
-from collections import defaultdict
 
 from hidet.ir.type import DataType, tensor_pointer_type
 from hidet.ir.expr import TensorElement, Expr, Var, SymbolVar, Constant, scalar_var, convert, cast
@@ -19,7 +18,7 @@ from hidet.ir.task import Task
 from hidet.ir.func import Function
 from hidet.ir.module import IRModule
 from hidet.ir.builders import FunctionBuilder, StmtBuilder
-from hidet.ir.functors import ExprRewriter, ExprVisitor, ComputeVisitor, ComputeRewriter, TypeRewriter, IRVisitor
+from hidet.ir.functors import ExprRewriter, ExprVisitor, ComputeVisitor, ComputeRewriter, TypeRewriter
 from hidet.ir.tools import IRPrinter, collect, rewrite, infer_type, simplify, collect_free_vars
 from hidet.ir.compute import ScalarInput, TensorInput, GridCompute, ReduceCompute, ArgReduceCompute
 from hidet.ir.compute import TensorNode, ScalarNode
@@ -31,26 +30,6 @@ from hidet.utils.namer import Namer
 
 class ScalarComputeFound(Exception):
     pass
-
-
-class UsageCounter(IRVisitor):
-    def __init__(self):
-        super().__init__()
-        self.usage: Dict[Union[TensorNode, ScalarNode], int] = defaultdict(int)
-
-    def visit_TensorInput(self, node: TensorInput):
-        self.usage[node] += 1
-
-    def visit_ScalarInput(self, node: ScalarInput):
-        self.usage[node] += 1
-
-    def visit_GridCompute(self, node: GridCompute):
-        self.usage[node] += 1
-        super().visit_GridCompute(node)
-
-    def visit_ReduceCompute(self, node: ReduceCompute):
-        self.usage[node] += 1
-        super().visit_ReduceCompute(node)
 
 
 class GridComputeInlineChecker(ExprVisitor, ComputeVisitor):
@@ -96,10 +75,6 @@ def can_inline_grid_compute(gc: GridCompute) -> bool:
 
 
 class GridComputeInliner(ExprRewriter, ComputeRewriter):
-    def __init__(self, usage_count: Dict[Union[TensorNode, ScalarNode], int]):
-        super().__init__()
-        self.usage_count: Dict[Union[TensorNode, ScalarNode], int] = usage_count
-
     def inline(self, node: TensorNode):
         return self.visit(node)
 
@@ -111,7 +86,7 @@ class GridComputeInliner(ExprRewriter, ComputeRewriter):
         indices = [self(index) for index in e.indices]
         if isinstance(base, GridCompute):
             assert isinstance(e.base, TensorNode)
-            if can_inline_grid_compute(base) or self.usage_count[e.base] == 1:
+            if can_inline_grid_compute(base):
                 return rewrite(base.value, {axis: index for axis, index in zip(base.axes, indices)})
         return ExprRewriter.visit_TensorElement(self, e)
 
@@ -143,10 +118,7 @@ def inline_grid_compute(nodes: List[TensorNode]) -> List[TensorNode]:
     ret: List[TensorNode]
         The nodes after inlining.
     """
-    usage_counter = UsageCounter()
-    usage_counter.visit(nodes)
-
-    inliner = GridComputeInliner(usage_counter.usage)
+    inliner = GridComputeInliner()
 
     return [inliner.inline(node) for node in nodes]
 

python/hidet/utils/stack_limit.py
@@ -17,18 +17,43 @@ import warnings
 import sys
 import resource
 
-# allow up to 512 MiB stack space
-expected_stack_size = 2**29  # 512 MiB
-stack_limit: Tuple[int, int] = resource.getrlimit(resource.RLIMIT_STACK)
-if stack_limit[1] != resource.RLIM_INFINITY and stack_limit[1] < expected_stack_size:
-    warnings.warn(
-        f'The hard limit for stack size is too small ({stack_limit[1] / 2**20:.1f} MiB), '
-        f'we recommend to increase it to {expected_stack_size / 2**20:.1f} MiB. '
-        'If you are the root user on Linux OS, you could refer to `man limits.conf` to increase this limit.'
-    )
-    resource.setrlimit(resource.RLIMIT_STACK, (stack_limit[1], stack_limit[1]))
-else:
-    resource.setrlimit(resource.RLIMIT_STACK, (expected_stack_size, stack_limit[1]))
 
-# allow up to 10^5 recursive python calls, increase this when needed
-sys.setrecursionlimit(100000)
+def set_stack_size(size: int = 2**29):  # 512 MiB
+    """
+    Set the stack size for python.
+
+    Parameters
+    ----------
+    size: int
+        The stack size for python, in bytes.
+    """
+    expected_stack_size = size
+    stack_limit: Tuple[int, int] = resource.getrlimit(resource.RLIMIT_STACK)
+    if stack_limit[1] != resource.RLIM_INFINITY and stack_limit[1] < expected_stack_size:
+        warnings.warn(
+            f'The hard limit for stack size is too small ({stack_limit[1] / 2**20:.1f} MiB), '
+            f'we recommend to increase it to {expected_stack_size / 2**20:.1f} MiB. '
+            'If you are the root user on Linux OS, you could refer to `man limits.conf` to increase this limit.'
+        )
+        resource.setrlimit(resource.RLIMIT_STACK, (stack_limit[1], stack_limit[1]))
+    else:
+        resource.setrlimit(resource.RLIMIT_STACK, (expected_stack_size, stack_limit[1]))
+
+
+def set_stack_limit(limit: int = 100000):  # 10^5 recursive calls
+    """
+    Set the stack limit for python.
+
+    Parameters
+    ----------
+    limit: int
+        The stack limit for python.
+    """
+    sys.setrecursionlimit(max(sys.getrecursionlimit(), limit))
+
+
+# allow more recursive python calls
+set_stack_limit()
+
+# allow more stack space
+set_stack_size()

tests/frontends/torch/models/test_torch_densenet121.py
@@ -18,7 +18,7 @@ from hidet.testing.torch_utils import check_module
 def test_densenet121(shape):
     model = torch.hub.load('pytorch/vision:v0.6.0', 'densenet121', pretrained=True).cuda().eval().to(torch.float16)
     x = torch.randn(*shape).cuda().to(torch.float16) * 0.1796 + 0.5491
-    check_module(model, [x], atol=2e-2, rtol=2e-2, dynamic=False)
+    check_module(model, [x], atol=4e-2, rtol=4e-2, dynamic=False)
 
 
 if __name__ == '__main__':