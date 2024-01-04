7b966ae38ef33122ee330561897b88f311ffc009
hjjq <hanjie.qiu@mail.utoronto.ca>
2024-01-04 12:03:29 -0500

    Add args to run script

--- .github/scripts/run_tests.py
+++ .github/scripts/run_tests.py
@@ -2,8 +2,8 @@ import os
 import json
 import subprocess
 import pathlib
-import numpy as np
-import tqdm
+import argparse
+from tabulate import tabulate
 from db_utils import get_db_conn
 
 external_models = ['llama-7b', 'gpt2']
@@ -38,11 +38,25 @@ def get_bench_cmd(run_type, run_id, run_name, run_param_name, dtype):
     return cmd
 
 if __name__ == '__main__':
-    fh = open('run_configs.json')
+    parser = argparse.ArgumentParser(prog='Run Benchmarks')
+    parser.add_argument(
+        '--print',
+        action='store_true',
+        default=False,
+        help='Print results'
+    )
+    parser.add_argument(
+        '--configs',
+        type=str,
+        default='run_configs.json',
+        help='Specify configurations file to use for benchmarking'
+    )
+    args = parser.parse_args()
+    configs_file = args.configs
+    fh = open(configs_file)
     run_configs = json.load(fh)
     fh.close()
     hw_config = os.environ.get('HW_CONFIG')
-    print('hw:', hw_config)
     for run_config in run_configs:
         # Append hardware_config column
         run_config['hardware_config'] = hw_config
@@ -62,5 +76,8 @@ if __name__ == '__main__':
             run_config['latency'] = latency
         else:
             run_config['latency'] = 999.99
-    with open('run_configs.json', 'w') as fh:
+    with open(configs_file, 'w') as fh:
         json.dump(run_configs, fh)
+
+    if args.print:
+       print(tabulate(run_configs, headers="keys")) 
