36580830d4403c1b152e88a9e470237c56eef5b7
Bolin Sun <bolin.sun@mail.utoronto.ca>
2024-04-05 20:54:13 -0400

    adding header guards

--- include/hidet/runtime/cpu/complex.h
+++ include/hidet/runtime/cpu/complex.h
@@ -9,6 +9,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#pragma once
+
 #include <complex>
 
 typedef std::complex<float> complex64_t;
--- include/hidet/runtime/cpu/float16.h
+++ include/hidet/runtime/cpu/float16.h
@@ -89,6 +89,8 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
+#pragma once
+
 #include <cmath>
 #include <cstring>
 #include <stdint.h>
