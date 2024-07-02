3f413bfa6c541de5ce1fff9655fd5f6d84e07b98
Bolin Sun <bolin.sun@mail.utoronto.ca>
2024-07-02 09:51:21 -0400

    [Operator] Extending the functionality support for `einsum` (#312)
    Closes #282 
    
    In the interest of time, only supported enough functionality to resolve
    the `NotImplementedError` in the issue.
    
    Also registered the `torch.Tensor.cumsum` method which was encountered
    in the same model immediately after extending `einsum`
    
    Additionally, added a default value `False` to the `inplace` parameter
    in the function `relu` defined in `register_functions.py` to match the
    behaviour in PyTorch

--- tests/operators/test_operator.py
+++ tests/operators/test_operator.py
@@ -11,6 +11,8 @@
 # limitations under the License.
 import hidet
 import pytest
+import torch
+import numpy as np
 
 hidet.option.save_lower_ir()
 
@@ -22,6 +24,34 @@ def test_profile_config():
     hidet.option.bench_config(1, 1, 1)
     c = hidet.ops.batch_matmul(a, b)
     hidet.option.search_space(0)
+
+
+@pytest.mark.parametrize(
+    "equation, operand_shapes",
+    [
+        ["bhwc,hkc->bhwk", [[400, 14, 14, 80], [14, 14, 80]]],
+        ["bhwc,wkc->bhwk", [[400, 14, 14, 80], [14, 14, 80]]],
+        ["bhwc,wkc->bhwk", [[16, 64, 64, 80], [64, 64, 80]]],
+        ["bhwc,hkc->bhwk", [[16, 64, 64, 80], [64, 64, 80]]],
+        ["ijk,k->ij", [[20, 30, 40], [40]]],
+        ["abc,c->ab", [[10, 20, 30], [30]]],
+        ["abcd,cd->ab", [[10, 20, 30, 40], [30, 40]]],
+        ["ij,j->i", [[10, 20], [20]]],
+        ["abc,bc->a", [[10, 20, 30], [20, 30]]],
+    ],
+)
+def test_einsum(equation, operand_shapes):
+    operands_torch = [torch.randn(shape) for shape in operand_shapes]
+    operands_hidet = [hidet.from_torch(op) for op in operands_torch]
+    result_torch = torch.einsum(equation, *operands_torch)
+    result_hidet = hidet.ops.einsum(equation, operands_hidet)
+
+    result_torch = result_torch.cpu().numpy()
+    result_hidet = result_hidet.cpu().numpy()
+
+    assert result_torch.shape == result_hidet.shape, "Output shape mismatch in einsum"
+
+    np.testing.assert_allclose(result_torch, result_hidet, atol=1e-2, rtol=1e-2)
 
 
 if __name__ == '__main__':
