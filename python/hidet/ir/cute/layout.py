3fba091970bea254eb973b171edeaa4c22b2bbac
xiaocenxiaocen <zx_math@yeah.net>
2024-07-02 16:02:12 -0400

    [Chore] replace copyrights with citations (#315)
    Closes #215 
    @wangshangsam @yaoyaoding 
    Is this what you expect in #215? Could you please kindly review this PR?
    Thanks.
    
    ---------
    
    Co-authored-by: xiaocenxiaocen <xiao.zhang@centml.ai>
    Co-authored-by: Shang Wang <samshang.wang@mail.utoronto.ca>

--- python/hidet/ir/cute/layout.py
+++ python/hidet/ir/cute/layout.py
@@ -1,4 +1,5 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
+
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -11,34 +12,8 @@
 # limitations under the License.
 
 ###################################################################################################
-# Copyright (c) 2023 - 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+# The following code is inspired by the core component, CuTe, from CUTLASS 3.5:
+# https://github.com/NVIDIA/cutlass/blob/main/python/pycute/layout.py
 ##################################################################################################/
 # This file is a python implementation for the layout (core concept) in CuTe, which will be
 # used for integrating CuTe dialect.
