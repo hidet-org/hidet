[Fix]  type casting for attention mask from fp32 -> f16 (#323)
-  Fixed cuda declaration and definition dtype mistmatch

- Added 3 more llms: mpt-7b, codellama-7b and mixtral-8x7b. Fitst two
are tested and working fine.

---------

Co-authored-by: Zhumakhan <nazirzhumakhan@gmail,.com>

python/hidet/testing/torch_utils.py
@@ -75,7 +75,7 @@ class Backend:
 
         hidet.torch.dynamo_config.use_tensor_core(True)
         hidet.option.cache_dir(hidet.option.get_cache_dir() + self.cache)
-
+        hidet.torch.dynamo_config.steal_weights(True)
         # hidet.option.cache_dir(hidet.option.get_cache_dir() + '/regression')
         # hidet.option.parallel_tune(max_parallel_jobs=1)
         # hidet.option.debug_cache_tuning(True)