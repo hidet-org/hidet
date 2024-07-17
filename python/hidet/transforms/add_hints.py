c014dab1a9ad7c9ade72da54ac12943e82215a0e
vadiklyutiy <156319763+vadiklyutiy@users.noreply.github.com>
2024-07-17 23:21:21 +0400

    [PERF] Introduce add_hint_pass (#355)
    Introduce `add_hint_pass`. It adds `__builtin_assume(...)` to .cu code
    that helps nvcc to understand bounds if `threadIdx` and `blockIdx` and
    optimize code better.
    
    **Performance improvements.** 
    Models
    model|latency|prev_latency|ratio|
    |--------|--------|--------|--------|
    bert-base-uncased|19.8138|20.2316|2.109
    densenet121|35.1161|36.7627|4.689
    efficientnet_b0|18.9451|19.278|1.757
    mobilenet_v2|11.5944|11.8764|2.432
    resnet50|29.4878|29.9935|1.715
    vit_b_16|125.787|123.672|-1.681
    
    Operators
    operator|latency|prev_latency|ratio
    |--------|--------|--------|--------|
    attn|1.50402|1.50131|-0.18
    attn|0.219707|0.227568|3.578
    attn_mask_add|1.5892|1.62516|2.263
    attn_mask_add|0.226317|0.226507|0.084
    batch_matmul|5.2399|5.11547|-2.375
    batch_matmul|0.0216016|0.0223425|3.43
    conv2d|0.0347093|0.0341758|-1.537
    conv2d|0.310521|0.308458|-0.664
    conv2d_gemm_f16|0.142542|0.146412|2.715
    conv2d_gemm_f16|2.0421|2.07043|1.387
    matmul_f16|2.22432|2.30458|3.608
    matmul_f16|0.00888628|0.00892615|0.449
    reduce|0.01375|0.0138618|0.813

---
+++ python/hidet/transforms/add_hints.py
@@ -0,0 +1,88 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import hidet
+
+from hidet.ir.func import Function
+from hidet.transforms import Pass
+from hidet.ir.functors import IRRewriter
+from hidet.ir.stmt import ForStmt, SeqStmt
+from hidet.ir.expr import Constant
+
+from hidet.ir.primitives.debug import __builtin_assume
+
+
+class AddHintsRewriter(IRRewriter):
+    def __init__(self):
+        super().__init__()
+        self.hint_body = None
+
+    def get_hints_body(self, func: Function):
+        from hidet.lang.cuda import threadIdx, blockIdx
+        from hidet.ir.analyzers import normalize_launch_dims
+
+        block_dims = normalize_launch_dims(func.attrs['cuda.block_dim'])
+        grid_dims = normalize_launch_dims(func.attrs['cuda.grid_dim'])
+
+        with hidet.script_module() as script_module:
+
+            @hidet.script
+            def _hint_func():
+                __builtin_assume(0 <= threadIdx.x)
+                __builtin_assume(threadIdx.x < block_dims[0])
+                __builtin_assume(0 <= threadIdx.y)
+                __builtin_assume(threadIdx.y < block_dims[1])
+                __builtin_assume(0 <= threadIdx.z)
+                __builtin_assume(threadIdx.z < block_dims[2])
+
+                __builtin_assume(0 <= blockIdx.x)
+                __builtin_assume(blockIdx.x < grid_dims[0])
+                __builtin_assume(0 <= blockIdx.y)
+                __builtin_assume(blockIdx.y < grid_dims[1])
+                __builtin_assume(0 <= blockIdx.z)
+                __builtin_assume(blockIdx.z < grid_dims[2])
+
+        hint = script_module.functions[0].body
+        self.hint_body = hint
+        return hint
+
+    def visit_Function(self, func: Function):
+        if func.kind != 'cuda_kernel':
+            return func
+        hint_body = self.get_hints_body(func)
+        body = self.visit(func.body)
+        body = SeqStmt((hint_body, body))
+        return Function(func.name, func.params, body, func.ret_type, func.kind, func.attrs)
+
+    # In perfect world __builtin_assume should be enought in the beggining of function only.
+    # But unfortunatelly sometimes nvcc lost this info. Seems that one case when the info is lost is
+    # after loops with not const extent.
+    # Add more __builtin_assume after such loop as a workaround.
+    def visit_ForStmt(self, loop: ForStmt):
+        body = self.visit(loop.body)
+        new_loop = ForStmt(loop.loop_var, loop.extent, body, attr=loop.attr)
+        if isinstance(loop.extent, Constant):
+            res = new_loop
+        else:
+            res = SeqStmt((new_loop, self.hint_body))
+        return res
+
+
+class AddHintsPass(Pass):
+    def process_func(self, func: Function) -> Function:
+        rewriter = AddHintsRewriter()
+        res = rewriter.visit(func)
+        return res
+
+
+def add_hints_pass() -> Pass:
+    return AddHintsPass()
