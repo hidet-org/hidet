de474126c8c07a6e1f0216b932dd29193022a3e4
Bolin Sun <bolin.sun@mail.utoronto.ca>
2024-10-21 21:47:12 -0400

    [Operators] Support `bfloat16` data type in `matmul` operator (#511)
    Closes #500

--- python/hidet/graph/ops/matmul/resolve.py
+++ python/hidet/graph/ops/matmul/resolve.py
@@ -195,6 +195,8 @@ class MatmulResolveRule(ResolveRule):
 
         transpose_b = op.attrs['transpose_b']
 
+        valid_dtypes = [dtypes.float16, dtypes.bfloat16]
+
         if not transpose_b and not (
             a.dtype.is_any_float16()
             and b.dtype.is_any_float16()
