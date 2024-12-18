4af9c5f89da700adf009b18438cae02b3a950f20
Vadim Gimpelson <vadim.gimpelson@centml.ai>
2024-12-19 03:55:46 +0400

    lint

--- tests/models/test_llama.py
+++ tests/models/test_llama.py
@@ -69,7 +69,6 @@ def test_llama2(device, opt):
     print(current_memory_pool("vcuda"))
 
 
-
 @pytest.mark.skip(
     reason='We now focus on the torch.compile API. '
     'The current llama model definition is not compatible huggingface thus disable the test.'
