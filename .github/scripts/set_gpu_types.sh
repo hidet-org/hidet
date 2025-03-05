d3ea7c6932c52e763c764575db52706f91c05632
Michael Wojcikiewicz <michael.wojcikiewicz@centml.ai>
2025-03-05 17:15:39 -0500

    [CI] fix test workflow gpu params for push event github action (#821)
    - after recently improving the Tests workflow to support triggering with
    multiple GPU types and weekly runs, assumptions had been made about what
    events can trigger the workflow.
    - this fix sets adds a case for handling push events from github
    
    (cherry picked from commit f88d8afd14bb1b77c92e517225af4c9ce1d2808d)

---
+++ .github/scripts/set_gpu_types.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# expects $INPUT_GPU_L4, $INPUT_GPU_H100, $INPUT_GPU_A10, $INPUT_GPU_A100
+echo "Selecting GPU types based on input or github event"
+echo "Triggered by event: $GITHUB_EVENT_NAME"
+if [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
+  echo "Using manual dispatch inputs"
+  GPU_L4="$INPUT_GPU_L4"
+  GPU_H100="$INPUT_GPU_H100"
+  GPU_A10="$INPUT_GPU_A10"
+  GPU_A100="$INPUT_GPU_A100"
+elif [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+  echo "Using pull request defaults"
+  GPU_L4="true"
+  GPU_H100="true"
+  GPU_A10="false"
+  GPU_A100="false"
+elif [ "$GITHUB_EVENT_NAME" == "push" ]; then
+  echo "Using push defaults"
+  GPU_L4="true"
+  GPU_H100="true"
+  GPU_A10="false"
+  GPU_A100="false"
+elif [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
+  echo "Using scheduled run defaults"
+  GPU_L4="true"
+  GPU_H100="true"
+  GPU_A10="true"
+  GPU_A100="true"
+else
+  echo "Unknown event type. Exiting."
+  exit 1
+fi
+
+# Write the parameters to GITHUB_OUTPUT so they can be used in later steps
+echo "gpu_l4=$GPU_L4" >> $GITHUB_OUTPUT
+echo "gpu_h100=$GPU_H100" >> $GITHUB_OUTPUT
+echo "gpu_a10=$GPU_A10" >> $GITHUB_OUTPUT
+echo "gpu_a100=$GPU_A100" >> $GITHUB_OUTPUT
