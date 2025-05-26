7ba17cce545b796324f238c3a15bbbcfcfd558ff
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-05-25 21:34:08 -0400

    [Fix] Wrapup the release
    1. Remove the `manylinux1` docker image for building hidet wheel. The
       main reason is hidet's dependency, torch, does not support this
       anymore.
    2. Remove "pygraphviz" from the dependency of hidet, since it only
       provides src on pypi. When building from source, it requires
       `graphviz-dev` package installed via `apt`, which makes
       `pip install .` fail.

--- scripts/wheel/build_wheel_manylinux_2_28_x86_64.sh
+++ scripts/wheel/build_wheel_manylinux_2_28_x86_64.sh
@@ -6,11 +6,11 @@ cd $SCRIPT_DIR
 
 # get the root directory of the project
 HIDET_DIR=$(cd -- "$SCRIPT_DIR/../.." &> /dev/null && pwd)
-
-# build the docker image
-bash ./dockerfiles/manylinux1/build_image.sh
-
 echo $HIDET_DIR
 
+# build the docker image
+ls ${SCRIPT_DIR}/dockerfiles/manylinux_2_28_x86_64/
+docker build -t hidet-manylinux_2_28_x86_64-build ${SCRIPT_DIR}/dockerfiles/manylinux_2_28_x86_64/
+
 # run the docker image
-docker run --rm -v $HIDET_DIR:/io hidet-manylinux1-build bash /io/scripts/wheel/build_wheel.sh $1
+docker run --rm -v $HIDET_DIR:/io hidet-manylinux_2_28_x86_64-build bash /io/scripts/wheel/build_wheel.sh $1
